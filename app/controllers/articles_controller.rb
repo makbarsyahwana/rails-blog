26d0bdcb5e4a4cea23fb8d525ffe98e01478c3d3
Muhammad Akbar Syahwana <makbarsyahwana@gmail.com>
2018-11-14 08:09:08 +0700

    rails blog

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,58 @@
+class ArticlesController < ApplicationController
+    
+    #To Get Form of New Article
+    def new
+        @article = Article.new
+    end
+
+    #Index All Article
+    def index
+        @articles = Article.all
+    end
+
+    #Create New Article
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    #Get Params of Edit Article
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    #Update Article
+    def update
+        @article = Article.find(params[:id])
+       
+        if @article.update(article_params)
+          redirect_to @article
+        else
+          render 'edit'
+        end
+    end
+
+    #Show Article
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    #Delete Article
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+       
+        redirect_to articles_path
+    end
+
+    #Get Params of Article
+    private
+        def article_params
+            params.require(:article).permit(:title, :text)
+        end
+end
